852f8e867c627dc1c76611b37ea0a0a4d7251348
hasstariq <68686454+hasstariq@users.noreply.github.com>
2020-08-01 23:21:20 -0400

    Update 170930500_181730940_a04.c

--- 170930500_181730940_a04.c
+++ 170930500_181730940_a04.c
@@ -1,13 +1,1 @@
 
-/**
- * CP386: Operating Systems
- * Assignment 4
- * 
- * Name: Hassan Tariq
- * GitHub: hasstariq
- * Student ID: 170930500
- * 
- * Name: Ravjot Singh
- * GitHub: 
- * Student ID: 
-**
